c95f507b0f1cea38fe4eff81237473fc157cc947
Steve Kinney <hello@stevekinney.net>
2019-05-29 08:14:06 -0500

    Remove implementation

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { decorate, observable, autorun } from 'mobx';
-import { observer } from 'mobx-react';
-
 import './styles.scss';
 
-class Count {
-  value = 0;
-  increment = () => {
-    this.value++;
-  };
-  decrement = () => {
-    this.value--;
-  };
-}
-
-decorate(Count, {
-  value: observable
-});
-
-const count = (window.count = new Count());
-
-const Counter = observer(({ count }) => {
+const Counter = ({ count }) => {
   return (
     <main className="Counter">
       <p className="count">{count.value}</p>
@@ -32,8 +13,6 @@ const Counter = observer(({ count }) => {
       </section>
     </main>
   );
-});
+};
 
-autorun(() => console.log('The count changed!', count.value));
-
-render(<Counter count={count} />, document.getElementById('root'));
+render(<Counter count={0} />, document.getElementById('root'));
